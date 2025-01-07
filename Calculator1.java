0f5f580775d5ebb65680d091b3e0e4c42459de5b
GaganTantwar <panthigagan478@gmail.com>
2025-01-07 13:23:47 +0530

    Addition

---
+++ Calculator1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Calculator1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Calculator");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Choose an operation: 1 for Addition, 2 for Subtraction, 3 for Multiplication, 4 for Division");
+        int choice = scanner.nextInt();
+        
+        System.out.println("Enter two numbers:");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                int sum = num1 + num2;
+                System.out.println("The sum is: " + sum);
+                break;
+            
+        }
+    }
+}
